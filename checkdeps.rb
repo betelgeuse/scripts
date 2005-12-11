e848c9e4823e96190891d4fcbc01e8bbac40c3fc
Petteri Räty <betelgeuse@gentoo.org>
2005-12-11 23:22:25 +0000

    Added ruby version of checkdeps.

---
+++ checkdeps.rb
@@ -0,0 +1,113 @@
+#!/usr/bin/ruby -w
+
+$verbose = false
+$debug = false
+
+pkgs_to_check=[]
+
+def print_help(exit_value)
+	puts 'Usage: checkdeps.rb opts|pkgs'
+	puts
+	puts 'Options:'
+	puts '-v, --verbose'
+	puts '-d, --debug'
+	puts '-h, --help'
+	puts
+	puts 'Everything else is passed to qfile as it is'
+	exit(exit_value)
+end
+
+ARGV.length == 0 && print_help(1)
+
+ARGV.each do | arg | 
+	if arg =~ /-v|--verbose/
+		$verbose = true
+	elsif arg =~ /-d|--debug/
+		$debug = true
+	elsif arg =~ /-h|--help/
+		print_help(0)
+	else
+		pkgs_to_check << arg
+	end
+end
+
+MAGIC=[127,69,76,70]
+
+def isElf(file)
+	if ! File.executable?(file) || ! File.file?(file)
+		if $verbose
+			puts "#{file} is not executable or a normal file"
+		end
+		return false
+	end
+
+	f = File.new(file)
+	i = 0
+	match=true
+	
+	while ( c = f.getc ) && i < 4
+		if c != MAGIC[i]
+			match=false
+		end
+		i=i+1
+	end
+
+	f.close
+
+	return match
+end
+
+def get_pkg_of_lib(lib)
+	`qfile -qC #{lib}`.chomp
+end
+
+def eval_line(pkg_hash,lib_hash,obj,line)
+	puts line if $debug
+	start = line.index('>')
+	puts "start",start if $debug
+	if start
+		start+=1
+		stop  = line.index('(',start)
+		puts "stop",stop if $debug
+		if stop
+			stop-=1
+			lib = line[start..stop]
+			lib.strip!
+
+			puts lib if $debug
+
+			return false if( lib == '' )
+
+			if File.exists?(lib)
+				pkg = get_pkg_of_lib(lib)
+				puts pkg if $debug
+				pkg_hash[pkg]=obj
+				return true
+			else
+				$stderr.print "Parsed #{lib} from the output of ldd"
+				$stderr.puts  "but no such file exists"
+				return false
+			end
+		end
+	end
+end
+
+libs = Hash.new
+pkgs = Hash.new
+
+qlist = IO.popen("qlist #{pkgs_to_check.join(' ')}")
+
+while obj = qlist.gets
+	obj.rstrip!
+	if isElf(obj)
+		ldd = IO.popen("ldd #{obj}")
+		ldd.each do | line | eval_line(pkgs,libs,obj,line) end
+	end
+end
+
+if $debug
+	require 'pp'
+	pp pkgs
+end
+
+puts pkgs.keys
