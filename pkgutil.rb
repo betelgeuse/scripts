2c7556da1681950b3c2f73db10a281ff222d6160
Petteri Räty <betelgeuse@gentoo.org>
2005-12-28 19:11:18 +0000

    Moved shared code from checkdeps.rb to pkgutil.rb.

---
+++ pkgutil.rb
@@ -0,0 +1,48 @@
+MAGIC="\x7FELF"
+def is_elf(file)
+	if ! File.executable?(file) || ! File.file?(file)
+		puts "#{file} is not executable or a normal file" if $verbose
+		return false
+	end
+
+	return File.read(file, 4) == MAGIC
+end
+
+def get_pkg_of_lib(lib)
+	command="qfile -qC #{lib}"
+	output = `#{command}`.chomp
+
+	if $? != 0
+		$stderr.puts "#{command} returned a non zero value."
+		return nil
+	end
+
+	puts command + ' :' + output if $DEBUG
+
+	output = output.split("\n").uniq.join(' || ')
+
+	puts 'Formatted: ' + output if $DEBUG
+
+	output
+end
+
+def handle_extra_output(prog)
+	$stderr.puts 'This program expects only one line'
+	$stderr.puts 'of output from scanelf. Extra lines:'
+	while line = scanelf.gets
+		$stderr.puts line
+	end
+	exit 2
+end
+
+def run_scanelf(elf)
+	scanelf = IO.popen("scanelf -q -F '%n#F' #{elf}")
+	first_line = scanelf.gets
+	handle_extra_output(scanelf) if not scanelf.eof?
+	scanelf.close
+
+	libs = first_line.split(',')
+	for lib in libs
+		yield lib
+	end	
+end
