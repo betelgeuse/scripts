96ce4fec7e6979e81332830b4aad8aaf5a56ad49
Petteri Räty <betelgeuse@gentoo.org>
2006-01-07 21:35:29 +0000

    New apicheck version that automatically finds packages to check.

---
+++ PackageLister.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.zip.*;
+
+public class PackageLister
+{
+    private static void populateSet(File jar, Set packages) throws IOException
+    {
+        ZipFile zipFile = new ZipFile(jar);
+        
+        for(Enumeration e = zipFile.entries(); e.hasMoreElements();)
+        {
+            ZipEntry entry = (ZipEntry) e.nextElement();
+            String path = entry.getName();
+            
+            if(path.endsWith(".class"))
+            {
+                String parentPath = new File(path).getParent();
+                packages.add(parentPath);
+            }
+        }
+    }
+    
+    public static void main(String[] args)
+    {
+        try
+        {
+            HashSet packages = new HashSet();
+            
+            for(int i = 0; i < args.length; i++)
+            {
+                String arg=args[i];
+                File jar = new File(arg);
+                populateSet(jar, packages);
+            }
+            
+            for(Iterator i = packages.iterator(); i.hasNext();)
+            {
+                String pkg = (String) i.next();
+                System.out.println(pkg.replaceAll("/","."));
+            }
+        }
+        
+        catch(IOException e)
+        {
+            System.err.println(e);
+            System.exit(1);
+        }
+    }
+}
