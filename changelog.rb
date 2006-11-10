e693cede29aab66330f5e0f5d5c47339faa2b61d
Petteri Räty <betelgeuse@gentoo.org>
2006-11-10 19:51:11 +0000

    Instead of hardcoding my mail use ECHANGELOG_USER

--- changelog.rb
+++ changelog.rb
@@ -3,8 +3,18 @@ def getLastChangeLogEntry()
 	f = File.new('ChangeLog')
 	entry=''
 
+	user = ENV['ECHANGELOG_USER']
+
+	if ! user
+		$stderr.puts('Need ECHANGELOG_USER env variable.')
+		$stderr.puts('See man echangelog.')
+		exit 1
+	end
+
+	puts user
+
    	while line = f.gets
-      	if(line.match(/betelgeuse@gentoo.org/))
+      	if(line.match(user))
 
            	while ! line.match(/:$/)
                	line = f.gets
